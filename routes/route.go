726643c66c5354f5ee083707b6e7bf21cc652686
Aditya Ibrar Abdillah <adityaibrar@gmail.com>
2025-08-02 20:53:12 +0700

    feat: create feature admin management venue

--- routes/route.go
+++ routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"sport-booking-backend/controllers"
+	"sport-booking-backend/utils"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -9,10 +10,21 @@ import (
 
 func SetupRoutes(app *fiber.App, db *gorm.DB) {
 	authController := controllers.NewAuthController(db)
+	venueController := controllers.NewVenueController(db)
 
 	api := app.Group("/api")
 	v1 := api.Group("/v1")
 
+	// Authentication
 	v1.Post("/register", authController.Register)
 	v1.Post("/login", authController.Login)
+
+	admin := v1.Group("/admin", utils.AuthMiddleware, utils.CheckRole)
+	// Venue
+	venue := admin.Group("/venue")
+	venue.Post("/", venueController.CreateVenue)
+	venue.Put("/:id", venueController.UpdateVenue)
+	venue.Get("/", venueController.GetListVenue)
+	venue.Get("/:id", venueController.GetDetailVenue)
+	venue.Delete("/:id", venueController.DeleteVenue)
 }
