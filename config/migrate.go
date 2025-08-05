dae25d975bcab1d4d5f3a04f41f3861fcc592f11
Aditya Ibrar Abdillah <adityaibrar@gmail.com>
2025-08-05 21:41:23 +0700

    feat: reset migrate

--- config/migrate.go
+++ config/migrate.go
@@ -7,6 +7,27 @@ import (
 	"gorm.io/gorm"
 )
 
+func ResetAndMigrate(db *gorm.DB) error {
+	models := []interface{}{
+		&models.User{},
+		&models.Venue{},
+		&models.Booking{},
+	}
+
+	if err := db.Migrator().DropTable(models...); err != nil {
+		log.Printf("Failed to drop tables: %v", err)
+		return err
+	}
+
+	if err := db.AutoMigrate(models...); err != nil {
+		log.Printf("Failed to auto migrate: %v", err)
+		return err
+	}
+
+	log.Println("Database reset and migration completed successfully")
+	return nil
+}
+
 func Migrate(db *gorm.DB) error {
 	err := db.AutoMigrate(
 		&models.User{},
